07a5c31176e4cd73128cca56a34879873a8d3a17
CristopherMartarello <crisrossi1313@gmail.com>
2024-01-24 21:54:29 -0300

    [PostGrid]

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -4,12 +4,12 @@ import { Heading } from '../Heading';
 import * as Styled from './styles';
 
 export type PostCardProps = {
+  id: string;
   title: string;
   cover: StrapiImage;
   excerpt: string;
   slug: string;
 };
-
 export const PostCard = ({ title, cover, excerpt, slug }: PostCardProps) => {
   return (
     <Styled.Wrapper>
@@ -18,7 +18,6 @@ export const PostCard = ({ title, cover, excerpt, slug }: PostCardProps) => {
           <Styled.Cover src={cover.url} alt={title} />
         </a>
       </Link>
-
       <Heading as="h2" size="small">
         <Link href={`/post/${slug}`}>
           <a>{title}</a>

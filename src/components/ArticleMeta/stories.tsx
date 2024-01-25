e8e49b48053552bcc4f1d70dc87d378980727ee9
CristopherMartarello <crisrossi1313@gmail.com>
2024-01-24 22:08:04 -0300

    [PostTags]

--- src/components/ArticleMeta/stories.tsx
+++ src/components/ArticleMeta/stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { ArticleMeta, ArticleMetaProps } from '.';
 import mock from './mock';
-
 export default {
   title: 'ArticleMeta',
   component: ArticleMeta,
@@ -16,7 +15,34 @@ export default {
     categories: { type: null },
   },
 } as Meta<ArticleMetaProps>;
+export const Template: Story<ArticleMetaProps> = (args) => {
+  return (
+    <div>
+      <ArticleMeta {...args} />
+    </div>
+  );
+};
 
-export const Template: Story<ArticleMetaProps> = (args) => (
-  <ArticleMeta {...args} />
-);
+export const NoAuthor: Story<ArticleMetaProps> = (args) => {
+  return (
+    <div>
+      <ArticleMeta {...args} author={undefined} />
+    </div>
+  );
+};
+
+export const NoCategories: Story<ArticleMetaProps> = (args) => {
+  return (
+    <div>
+      <ArticleMeta {...args} categories={undefined} />
+    </div>
+  );
+};
+
+export const NoAuthorAndCategories: Story<ArticleMetaProps> = (args) => {
+  return (
+    <div>
+      <ArticleMeta {...args} categories={undefined} author={undefined} />
+    </div>
+  );
+};
